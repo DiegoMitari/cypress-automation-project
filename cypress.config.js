319da5e54b7d572c148014a082e2e320145df50f
diegomitari <diego89ma@gmail.com>
2023-12-01 19:24:45 -0500

    fix type to invoke

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
+  "retries": {
+    "openMode": 2,   // Número de intentos para comandos de apertura (visit, get, etc.)
+    "runMode": 3     // Número de intentos para comandos durante la ejecución de la prueba
+  } ,
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
